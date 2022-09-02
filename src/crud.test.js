85c82f3b2688b84b3cab3bb4f8dbf5f677d75f37
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-09-02 21:53:25 +0430

    Test updateStatus method

--- src/crud.test.js
+++ src/crud.test.js
@@ -1,30 +1,30 @@
 const storeTodo = require('./crud.js');
 
-// Unit test for add todo
+// Unit test to test saveTodo method
 describe('addTodo()', () => {
   it('addTodo must add a todo in the existing list', () => {
     // Arrange
     const todos = [
       {
         index: 1,
-        description: 'one',
+        description: 'First todo',
         isCompeleted: false,
       },
     ];
     const newTodo = {
       index: 2,
-      description: 'two',
+      description: 'Second todo',
       isCompeleted: true,
     };
     const expected = [
       {
         index: 1,
-        description: 'one',
+        description: 'First todo',
         isCompeleted: false,
       },
       {
         index: 2,
-        description: 'two',
+        description: 'Second todo',
         isCompeleted: true,
       },
     ];
@@ -35,7 +35,7 @@ describe('addTodo()', () => {
   });
 });
 
-// Unit-Test for deleting a task
+// Unit test to test removeTodo method
 describe('removeTodo()', () => {
   it('removeTodo must remove todo from existing list', () => {
     // Arrange
@@ -65,6 +65,78 @@ describe('removeTodo()', () => {
     ];
     // Act
     const output = storeTodo.removeTodo(tobeDeleted.index, todos);
+    // Assert
+    expect(output).toStrictEqual(expected);
+  });
+});
+
+// Unit test for editing todo description function.
+describe('updateDescription()', () => {
+  it('updateDescription must update description', () => {
+    // Arrange
+    const todos = [
+      {
+        index: 1,
+        description: 'to be updated',
+        isCompeleted: true,
+      },
+      {
+        index: 2,
+        description: 'This description should not be updated',
+        isCompeleted: false,
+      },
+    ];
+
+    const expected = [
+      {
+        index: 1,
+        description: 'Updated description',
+        isCompeleted: true,
+      },
+      {
+        index: 2,
+        description: 'This description should not be updated',
+        isCompeleted: false,
+      },
+    ];
+    // Act
+    const output = storeTodo.updateDescription(1, 'Updated description', todos);
+    // Assert
+    expect(output).toStrictEqual(expected);
+  });
+});
+
+// Unit test to test updateStatus method
+describe('updateStatus()', () => {
+  it('updateStatus method must update todo status to true', () => {
+    // Arrange
+    const todos = [
+      {
+        index: 1,
+        description: 'First',
+        isCompeleted: false,
+      },
+      {
+        index: 2,
+        description: 'Second',
+        isCompeleted: false,
+      },
+    ];
+
+    const expected = [
+      {
+        index: 1,
+        description: 'First',
+        isCompeleted: true,
+      },
+      {
+        index: 2,
+        description: 'Second',
+        isCompeleted: false,
+      },
+    ];
+    // Act
+    const output = storeTodo.updateStatus(1, true, todos);
     // Assert
     expect(output).toStrictEqual(expected);
   });
